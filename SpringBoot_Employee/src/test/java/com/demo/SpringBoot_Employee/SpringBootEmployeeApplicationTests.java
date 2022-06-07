713b20f09533352b62acf6570adadb1389a4f310
Matthew Pachapa <pachapa36@gmail.com>
2022-06-07 08:32:07 -0400

    initial commit

---
+++ SpringBoot_Employee/src/test/java/com/demo/SpringBoot_Employee/SpringBootEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.SpringBoot_Employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
