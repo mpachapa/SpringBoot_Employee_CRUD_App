43d9b07c0baabd87814264593b1caeaea8efaccb
Matthew Pachapa <pachapa36@gmail.com>
2022-06-22 13:57:48 -0400

    removed sprinboot_employee

--- SpringBoot_Employee/src/main/java/com/demo/SpringBoot_Employee/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.demo.SpringBoot_Employee.repository;
-
-import com.demo.SpringBoot_Employee.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository <Employee,Long>{
-
-}
